af3ef99e4901d80884c6d1524e780800bcb208c5
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-06-25 16:30:47 +0600

    Time: 8 ms (43.62%), Space: 26.7 MB (99.09%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+        
+        if (n <= 1) return true;
+
+        for (int i = 0; i < n - 1; i++) {
+            
+            if ((nums[i] % 2) == (nums[i + 1] % 2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
