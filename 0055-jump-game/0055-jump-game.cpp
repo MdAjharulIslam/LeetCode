9b0e34813e0535ef097c23473cc240441729536f
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-09-05 12:01:34 +0600

    Time: 44 ms (57.71%), Space: 51.2 MB (41.24%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int maxr=0;
+        for(int i=0;i<n;i++){
+               if(maxr<i)return false;
+            
+            maxr=max(maxr,nums[i]+i);
+        }
+    return true; 
+        
+        
+        
+    }
+};
