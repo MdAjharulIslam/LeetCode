0b8503041e43b20b9c37ee3d03658c80b98e8781
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-10-05 00:01:04 +0600

    Time: 11 ms (59.42%), Space: 13.8 MB (45.66%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows <= 1) {
+            return s;
+        }
+        vector<string> list(numRows, "");
+        int currentLine = 0;
+        bool reachedEdge = true;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (currentLine == 0 || currentLine == numRows - 1) {
+                reachedEdge = !reachedEdge;
+            }
+            list.at(currentLine) += s.at(i);
+            if (!reachedEdge) {
+                currentLine++;
+            } else {
+                currentLine--;
+            }
+        }
+
+        string result;
+        for (int i = 0; i < numRows; ++i) {
+            result += list.at(i);
+        }
+        return result;
+    }
+};
