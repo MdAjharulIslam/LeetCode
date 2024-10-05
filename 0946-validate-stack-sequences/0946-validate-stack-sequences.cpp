f848823d1223ea5c2082888da9b3ed86e9227600
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-10-06 00:23:22 +0600

    Time: 8 ms (48.96%), Space: 18.3 MB (30.36%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -4,11 +4,15 @@ public:
         
         stack<int>s;
         int i=0;
-        for (int j=0;j<pushed.size();++j) {
+        for (int j=0;j<pushed.size();j++) {
             s.push(pushed[j]);
             
            
-            while (!s.empty() && s.top() == popped[i]) s.pop(), ++i;
+            while (!s.empty() && s.top() == popped[i])
+            {
+                s.pop();
+                i++;
+            }
         }
         
         
