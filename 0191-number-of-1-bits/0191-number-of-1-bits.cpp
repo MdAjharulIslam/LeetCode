c33ba9f002dea74a85fcca000ae9c6f884bce9a7
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2024-11-29 22:40:42 +0600

    Time: 0 ms (100.00%), Space: 8.3 MB (6.12%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int ans=0;
+        while(n!=0){
+            if(n&1==1){
+                ans++;
+            }
+            n=(n>>1);
+        }
+        return ans;
+    }
+};
