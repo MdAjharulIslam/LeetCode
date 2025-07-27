573937284529b797bbc8bbac77a2cbf3391fce13
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2025-07-27 15:19:52 +0600

    Time: 46 ms (22.03%), Space: 53.8 MB (12.9%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+ const greeting = "Hello World"
+
+    return function(...args) {
+        return greeting;
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
