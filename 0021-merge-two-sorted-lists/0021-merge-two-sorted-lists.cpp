39015beeed06e64bc2e21b3c6fb2210e6aad196e
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2025-12-06 19:52:03 +0600

    Time: 0 ms (100%), Space: 19.5 MB (27.63%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -12,25 +12,51 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         
-     ListNode *ptr1 = list1;
-     ListNode *ptr2 = list2;
+    //  ListNode *ptr1 = list1;
+    //  ListNode *ptr2 = list2;
 
-     if(ptr1 == NULL){
-        return ptr2;
-     }
-     if(ptr2== NULL){
-        return ptr1;
-     }
+    //  if(ptr1 == NULL){
+    //     return ptr2;
+    //  }
+    //  if(ptr2== NULL){
+    //     return ptr1;
+    //  }
 
-     if(ptr1->val < ptr2->val){
-        ptr1->next = mergeTwoLists(ptr1->next, ptr2);
-        return ptr1;
-     }
+    //  if(ptr1->val < ptr2->val){
+    //     ptr1->next = mergeTwoLists(ptr1->next, ptr2);
+    //     return ptr1;
+    //  }
 
-        else{
-            ptr2->next = mergeTwoLists(ptr1, ptr2->next);
-            return ptr2;
+    //     else{
+    //         ptr2->next = mergeTwoLists(ptr1, ptr2->next);
+    //         return ptr2;
+    //     }
+
+   ListNode dummy;
+        ListNode* tail = &dummy;
+
+        ListNode* ptr1 = list1;
+        ListNode* ptr2 = list2;
+
+        
+        while (ptr1 != nullptr && ptr2 != nullptr) {
+            if (ptr1->val < ptr2->val) {
+                tail->next = ptr1;
+                ptr1 = ptr1->next;
+            } else {
+                tail->next = ptr2;
+                ptr2 = ptr2->next;
+            }
+            tail = tail->next; 
         }
 
+        
+        if (ptr1 != nullptr) {
+            tail->next = ptr1;
+        } else {
+            tail->next = ptr2;
+        }
+
+        return dummy.next;
     }
 };
