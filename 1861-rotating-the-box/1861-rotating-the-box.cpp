6c084c32e7f13ee7913dba011e200a806a955d20
Md_Ajharul_Islam <167879829+MdAjharulIslam@users.noreply.github.com>
2026-05-06 10:42:35 +0600

    Time: 15 ms (41.52%), Space: 56.8 MB (69.53%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& grid) {
+        int rows = grid.size(), cols = grid[0].size();
+        for (int r = 0; r < rows; r++) {
+            int p = 0;
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '.') {
+                    swap(grid[r][c], grid[r][p]);
+                    p++;
+                } else if (grid[r][c] == '*')
+                    p = c + 1;
+            }
+        }
+        
+        vector<vector<char>> res(cols, vector<char>(rows));
+        for (int r = 0; r < rows; r++)
+            for (int c = 0; c < cols; c++)
+                res[c][rows - 1 - r] = grid[r][c];
+                
+        return res;
+    }
+};
